fe2ee95b51ae2d199048ece627e006fe3a876a2d
Chertenko <kchek816@gmail.com>
2024-03-29 10:53:53 +0300

    init project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@constants': '/src/constants',
+      '@pages': '/src/pages',
+      '@store': '/src/store',
+      '@utils': '/src/utils',
+    },
+  },
+})

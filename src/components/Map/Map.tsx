c8554411b87b79f46a37ddf00b010886d8db954a
Chertenko <kchek816@gmail.com>
2024-03-29 17:41:39 +0300

    feat: Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,35 @@
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+import styles from './Map.module.css'
+import { useState } from 'react'
+import { MapHeader } from './MapHeader/MapHeader'
+
 export const Map = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: import.meta.env.VITE_API_KEY,
+  })
+
+  const [map, setMap] = useState<google.maps.Map | null>(null)
+
+  const center = {
+    lat: 53.675284,
+    lng: 23.829562,
+  }
+
   return (
-    <article>
-      <div>1</div>
+    <article className={styles.map}>
+      {isLoaded && (
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ flexGrow: 1 }}
+          options={{ mapTypeControl: false, streetViewControl: false }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+        </GoogleMap>
+      )}
+      <MapHeader />
     </article>
   )
 }

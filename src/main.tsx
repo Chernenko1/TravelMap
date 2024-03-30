ddae23a32ab0f04a88fcda713dc0f22d50a51575
Chertenko <kchek816@gmail.com>
2024-03-30 20:09:21 +0300

    feat: New Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './fonts/Mont/Mont-Regular.ttf'
 import App from './App.tsx'
 import ErrorBoundary from '@components/ErrorBoundary/ErrorBoundary.tsx'
 

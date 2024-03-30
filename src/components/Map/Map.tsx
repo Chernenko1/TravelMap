878c8bced89b3899d39c4388ca94d7e8959ae62e
Chertenko <kchek816@gmail.com>
2024-03-30 10:20:56 +0300

    fix: deleted react-google-maps

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,34 +1,15 @@
-import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
 import styles from './Map.module.css'
-import { useState } from 'react'
+import { MutableRefObject, useRef, useState } from 'react'
 import { MapHeader } from './MapHeader/MapHeader'
 
+const center = {
+  lat: 53.675284,
+  lng: 23.829562,
+}
+
 export const Map = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: import.meta.env.VITE_API_KEY,
-  })
-
-  const [map, setMap] = useState<google.maps.Map | null>(null)
-
-  const center = {
-    lat: 53.675284,
-    lng: 23.829562,
-  }
-
   return (
     <article className={styles.map}>
-      {isLoaded && (
-        <GoogleMap
-          center={center}
-          zoom={15}
-          mapContainerStyle={{ flexGrow: 1 }}
-          options={{ mapTypeControl: false, streetViewControl: false }}
-          onLoad={(map) => setMap(map)}
-        >
-          <Marker position={center} />
-        </GoogleMap>
-      )}
       <MapHeader />
     </article>
   )
--- src/components/Map/MapHeader/MapHeader.tsx
+++ src/components/Map/MapHeader/MapHeader.tsx
@@ -1,20 +1,25 @@
 import { IoNavigateCircleOutline } from 'react-icons/io5'
 import styles from './MapHeader.module.css'
 
-export const MapHeader = () => {
+interface IMapHeader {}
+
+export const MapHeader = ({}: IMapHeader) => {
   return (
     <article className={styles.mapHeader}>
+      <section className={styles.inputContainer}>
+        <input type='text' />
+
+        <input type='text' />
+      </section>
       <section>
-        <input type='text' />
-        <input type='text' />
         <button type='submit'>Search</button>
-        <button className={styles.centeringButton} onClick={() => console.log(1)}>
+        <button className={styles.centeringButton}>
           <IoNavigateCircleOutline size={24} color='red' style={{ position: 'relative', top: 7 }} />
         </button>
       </section>
       <section className={styles.infoSection}>
-        <p>AAA</p>
-        <p>BBB</p>
+        <p>Duration: {}</p>
+        <p>Distance : {}</p>
       </section>
     </article>
   )

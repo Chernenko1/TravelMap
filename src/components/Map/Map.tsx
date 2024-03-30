69becd7e25114fa8a29572eb4248415d6d921de4
Chertenko <kchek816@gmail.com>
2024-03-30 10:45:36 +0300

    feat: use ReactLeaflet

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,7 @@
 import styles from './Map.module.css'
-import { MutableRefObject, useRef, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { MapHeader } from './MapHeader/MapHeader'
+import 'leaflet/dist/leaflet.css'
 
 const center = {
   lat: 53.675284,
@@ -10,6 +11,20 @@ const center = {
 export const Map = () => {
   return (
     <article className={styles.map}>
+      <MapContainer
+        center={center}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: '90vh', width: '100%', zIndex: 0 }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={center}>
+          <Popup>Жопа</Popup>
+        </Marker>
+      </MapContainer>
       <MapHeader />
     </article>
   )

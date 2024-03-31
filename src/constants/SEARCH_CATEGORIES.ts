e99f05bfbaca20a34af41c21d0dee40f32ec4424
Chertenko <kchek816@gmail.com>
2024-03-31 21:33:06 +0300

    feat: consts

---
+++ src/constants/SEARCH_CATEGORIES.ts
@@ -0,0 +1,79 @@
+export const SEARCH_CATEGORIES: { title: string; value: string }[] = [
+  {
+    title: 'Место для сна',
+    value: 'accommodation',
+  },
+  {
+    title: 'Спорт',
+    value:
+      'activity,entertainment.activity_park.climbing,entertainment.activity_park.trampoline,entertainment.activity_park, building.sport,sport',
+  },
+  {
+    title: 'Индустриальные объекты',
+    value: 'airport,power,railway',
+  },
+  {
+    title: 'Природа',
+    value: 'natural,national_park',
+  },
+  {
+    title: 'Авто',
+    value: 'rental.car',
+  },
+  {
+    title: 'Велосипеды',
+    value: 'rental.bicycle',
+  },
+  {
+    title: 'Магазины',
+    value:
+      'commercial.food_and_drink, commercial.supermarket,commercial.marketplace,commercial.shopping_mall,commercial.discount_store,commercial.kiosk',
+  },
+  {
+    title: 'Еда',
+    value: 'catering',
+  },
+  {
+    title: 'Заправки',
+    value: 'commercial.gas',
+  },
+  {
+    title: 'Кофе/чай',
+    value: 'catering.cafe',
+  },
+  {
+    title: 'Банки',
+    value: 'service.financial',
+  },
+  {
+    title: 'Развлечения (парки, колеса обозрения, прочие аттракционы)',
+    value: `entertainment.zoo,entertainment.aquarium,entertainment.planetarium,entertainment.cinema,amusement_arcade,entertainment.escape_game,
+      entertainment.miniature_golf,entertainment.bowling_alley,flying_fox,entertainment.water_park,entertainment.theme_park`,
+  },
+  {
+    title: 'Разное (смотровые площадки и неклассифицированные достопримечаительности)',
+    value: 'camping,leisure',
+  },
+  {
+    title: 'Архитектура (историческая(пирамиды амфитеатры дворцы арки и дра), мосты башни маяки) ',
+    value: 'tourism.sights.bridge,tourism.sights.castle',
+  },
+  {
+    title: 'Религия',
+    value: 'tourism.sights.place_of_worship,tourism.sights.monastery,religion',
+  },
+  {
+    title:
+      'История (в т.ч оборонительные сооружения(крепости и замки городища башни бункеры) и археология, монументы и мемориалы)',
+    value:
+      'building.historic,tourism.sights.memorial,tourism.sights.battlefield,tourism.sights.fort,,tourism.sights.ruines',
+  },
+  {
+    title: '18+',
+    value: 'commercial.erotic, adult',
+  },
+  {
+    title: 'Культура (музеи, театры, городская среда, искусство)',
+    value: 'entertainment.culture',
+  },
+]

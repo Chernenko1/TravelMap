83389dabd93754dcd11a7b35b4729fcddd93aa4f
Chertenko <kchek816@gmail.com>
2024-04-01 16:36:12 +0300

    added constant

--- src/constants/SEARCH_CATEGORIES.ts
+++ src/constants/SEARCH_CATEGORIES.ts
@@ -1,79 +1,115 @@
-export const SEARCH_CATEGORIES: { title: string; value: string }[] = [
+import Accommodation from '@assets/svg/accommodation.svg'
+import Sport from '@assets/svg/sport.svg'
+import Industrial from '@assets/svg/industrial.svg'
+import Nature from '@assets/svg/nature.svg'
+import Cars from '@assets/svg/cars.svg'
+import Bike from '@assets/svg/bike.svg'
+import Shops from '@assets/svg/shops.svg'
+import Food from '@assets/svg/food.svg'
+import Gas from '@assets/svg/gas.svg'
+import Cafe from '@assets/svg/cafe.svg'
+import Banks from '@assets/svg/cafe.svg'
+import Entertainment from '@assets/svg/entertainment.svg'
+import Other from '@assets/svg/other.svg'
+import Architecture from '@assets/svg/architecture.svg'
+import Religion from '@assets/svg/religion.svg'
+import History from '@assets/svg/history.svg'
+import Adults from '@assets/svg/18+.svg'
+import Culture from '@assets/svg/culture.svg'
+
+export const SEARCH_CATEGORIES: { title: string; value: string; icon: string }[] = [
   {
+    icon: Accommodation,
     title: 'Место для сна',
     value: 'accommodation',
   },
   {
+    icon: Sport,
     title: 'Спорт',
     value:
       'activity,entertainment.activity_park.climbing,entertainment.activity_park.trampoline,entertainment.activity_park, building.sport,sport',
   },
   {
+    icon: Industrial,
     title: 'Индустриальные объекты',
     value: 'airport,power,railway',
   },
   {
+    icon: Nature,
     title: 'Природа',
     value: 'natural,national_park',
   },
   {
+    icon: Cars,
     title: 'Авто',
     value: 'rental.car',
   },
   {
+    icon: Bike,
     title: 'Велосипеды',
     value: 'rental.bicycle',
   },
   {
+    icon: Shops,
     title: 'Магазины',
     value:
       'commercial.food_and_drink, commercial.supermarket,commercial.marketplace,commercial.shopping_mall,commercial.discount_store,commercial.kiosk',
   },
   {
+    icon: Food,
     title: 'Еда',
     value: 'catering',
   },
   {
+    icon: Gas,
     title: 'Заправки',
     value: 'commercial.gas',
   },
   {
+    icon: Cafe,
     title: 'Кофе/чай',
     value: 'catering.cafe',
   },
   {
+    icon: Banks,
     title: 'Банки',
     value: 'service.financial',
   },
   {
-    title: 'Развлечения (парки, колеса обозрения, прочие аттракционы)',
+    icon: Entertainment,
+    title: 'Развлечения',
     value: `entertainment.zoo,entertainment.aquarium,entertainment.planetarium,entertainment.cinema,amusement_arcade,entertainment.escape_game,
       entertainment.miniature_golf,entertainment.bowling_alley,flying_fox,entertainment.water_park,entertainment.theme_park`,
   },
   {
-    title: 'Разное (смотровые площадки и неклассифицированные достопримечаительности)',
+    icon: Other,
+    title: 'Разноe',
     value: 'camping,leisure',
   },
   {
-    title: 'Архитектура (историческая(пирамиды амфитеатры дворцы арки и дра), мосты башни маяки) ',
+    icon: Architecture,
+    title: 'Архитектура',
     value: 'tourism.sights.bridge,tourism.sights.castle',
   },
   {
+    icon: Religion,
     title: 'Религия',
     value: 'tourism.sights.place_of_worship,tourism.sights.monastery,religion',
   },
   {
-    title:
-      'История (в т.ч оборонительные сооружения(крепости и замки городища башни бункеры) и археология, монументы и мемориалы)',
+    icon: History,
+    title: 'История',
     value:
-      'building.historic,tourism.sights.memorial,tourism.sights.battlefield,tourism.sights.fort,,tourism.sights.ruines',
+      'building.historic,tourism.sights.memorial,tourism.sights.battlefield,tourism.sights.fort,tourism.sights.ruines',
   },
   {
+    icon: Adults,
     title: '18+',
     value: 'commercial.erotic, adult',
   },
   {
-    title: 'Культура (музеи, театры, городская среда, искусство)',
+    icon: Culture,
+    title: 'Культура',
     value: 'entertainment.culture',
   },
 ]

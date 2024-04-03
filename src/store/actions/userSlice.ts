c36e8553ed3cc2e6b97047d79d2a5597304d0b60
Chertenko <kchek816@gmail.com>
2024-04-03 16:21:47 +0300

    fix: deleted variables

--- src/store/actions/userSlice.ts
+++ src/store/actions/userSlice.ts
@@ -2,38 +2,26 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '@store/store'
 
 interface IUserSlice {
-  isAuth: boolean
-  email: string
-  name: string
-  id: string
-  token: string
+  email: string | null
+  id: string | null
 }
 
 const initialState: IUserSlice = {
-  isAuth: false,
-  email: '',
-  name: '',
-  token: '',
-  id: '',
+  email: null,
+  id: null,
 }
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUser: (state, action: PayloadAction<{ id: string; email: string; token: string; name: string }>) => {
-      state.isAuth = true
+    setUser: (state, action: PayloadAction<IUserSlice>) => {
       state.id = action.payload.id
       state.email = action.payload.email
-      state.token = action.payload.token
-      state.name = action.payload.name
     },
     removeUser: (state) => {
-      state.isAuth = false
-      state.id = ''
-      state.email = ''
-      state.token = ''
-      state.name = ''
+      state.id = null
+      state.email = null
     },
   },
 })

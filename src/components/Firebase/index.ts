843b712fed43873b999d8a9cf145c5c74c5c53a7
Chertenko <kchek816@gmail.com>
2024-04-08 14:26:13 +0300

    feat: init firestore

--- src/components/Firebase/index.ts
+++ src/components/Firebase/index.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app'
 import { env } from '@constants/index'
 import { getAnalytics } from 'firebase/analytics'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: env.VITE_FIREBASE_API_KEY,
@@ -14,5 +15,7 @@ const firebaseConfig = {
 
 const FireBaseApp = initializeApp(firebaseConfig)
 const analytics = getAnalytics(FireBaseApp)
+
+export const database = getFirestore(FireBaseApp)
 
 export default FireBaseApp

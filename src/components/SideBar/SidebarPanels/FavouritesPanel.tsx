1b759e3bddc2e122a18d580ec8def98af58e6be7
Chertenko <kchek816@gmail.com>
2024-04-12 16:13:51 +0300

    feat: warning text

--- src/components/SideBar/SidebarPanels/FavouritesPanel.tsx
+++ src/components/SideBar/SidebarPanels/FavouritesPanel.tsx
@@ -3,9 +3,12 @@ import { Input } from '@components/Input/Input'
 import { ScrollMenu } from '@components/ScrollMenu/ScrollMenu'
 import styles from './FavouritesPanel.module.css'
 import { useAppSelector } from '@store/hooks'
+import { useAuth } from '@hooks/useAuth'
+import { NotLogIn } from '@components/Warnings/NotLogIn'
 
 export const FeaturesPanel = () => {
   const places = useAppSelector((state) => state.favPlaces.places)
+  const { isAuth } = useAuth()
 
   return (
     <div className={styles.featuresPanel}>
@@ -15,14 +18,26 @@ export const FeaturesPanel = () => {
 
       <div className={styles.infoContainer}>
         <p>Избранное: </p>
-        <ScrollMenu>
-          <div className={styles.cards}>
-            {places.map((place) => (
-              <PanelCard key={place.placeId} place={place} />
-            ))}
-          </div>
-        </ScrollMenu>
+        {isAuth ? (
+          <ScrollMenu>
+            <div className={styles.cards}>
+              {places.map((place) => (
+                <PanelCard key={place.placeId} place={place} />
+              ))}
+            </div>
+          </ScrollMenu>
+        ) : (
+          <NotLogIn />
+        )}
       </div>
     </div>
   )
 }
+
+// <ScrollMenu>
+//  <div className={styles.cards}>
+//    {places.map((place) => (
+//     <PanelCard key={place.placeId} place={place} />
+//   ))}
+//  </div>
+//  </ScrollMenu>

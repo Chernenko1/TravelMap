f1edd3d6946ab68c77224bbdbb8155f8c89fddf7
Chertenko <kchek816@gmail.com>
2024-04-12 15:48:13 +0300

    feat: added new key

--- src/constants/firebase.ts
+++ src/constants/firebase.ts
@@ -2,4 +2,5 @@ export const firebaseErrors = {
   'auth/too-many-requests': 'Слишком много запросов попробуйте позже',
   'auth/invalid-credential': 'Почта или пароль введены неверно',
   'auth/invalid-email': 'Некорректный адрес почты',
+  'auth/email-already-in-use': 'Данный адрес почты уже занят',
 }
